8d27175f06e6915449eaad665475010c3b7f6bad
chulnwza <64070019@it.kmitl.ac.th>
2021-10-26 00:59:01 +0700

    message

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+
+img1 = cv2.imread('image/sample2.png')
+img2 = img1
+
+rad = int(input())
+def checkposition(event, x, y, flags, param):
+    """checkposition"""
+    if event == cv2.EVENT_LBUTTONDOWN:
+        blue = int((int(img1[y+(rad + 1), x, 0]) + int(img1[y-(rad + 1), x, 0]) + int(img1[y, x-(rad + 1), 0]) + int(img1[y, x+(rad + 1), 0]))/4)
+        green = int((int(img1[y+(rad + 1), x, 1]) + int(img1[y-(rad + 1), x, 1]) + int(img1[y, x-(rad + 1), 1]) + int(img1[y, x+(rad + 1), 1]))/4)
+        red = int((int(img1[y+(rad + 1), x, 2]) + int(img1[y-(rad + 1), x, 2]) + int(img1[y, x-(rad + 1), 2]) + int(img1[y, x+(rad + 1), 2]))/4)
+        cv2.circle(img1, (x, y), rad, (int(blue), int(green), int(red)), -1)
+        cv2.imshow('Image', img1)
+
+cv2.imshow('Image', img1)
+cv2.imshow('Before', img2)
+cv2.setMouseCallback('Image', checkposition)
+#cv2.imwrite('Image.jpg', img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
