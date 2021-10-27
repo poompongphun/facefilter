fc7ed2464e7da25d93e31d899004e004ce162742
YolradeeIT <64070089@kmitl.ac.th>
2021-10-27 17:16:15 +0700

    commit main 8 pot

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import cv2
+import math as m
 
-img1 = cv2.imread('image/sample2.png')
+img1 = cv2.imread('image/BTsample1.jpg')
 img2 = img1
 
-rad = int(input())
+rad = int(input()) + 1
+cossin = int(rad*(2**0.5/2))
 def checkposition(event, x, y, flags, param):
     """checkposition"""
     if event == cv2.EVENT_LBUTTONDOWN:
-        blue = int((int(img1[y+(rad + 1), x, 0]) + int(img1[y-(rad + 1), x, 0]) + int(img1[y, x-(rad + 1), 0]) + int(img1[y, x+(rad + 1), 0]))/4)
-        green = int((int(img1[y+(rad + 1), x, 1]) + int(img1[y-(rad + 1), x, 1]) + int(img1[y, x-(rad + 1), 1]) + int(img1[y, x+(rad + 1), 1]))/4)
-        red = int((int(img1[y+(rad + 1), x, 2]) + int(img1[y-(rad + 1), x, 2]) + int(img1[y, x-(rad + 1), 2]) + int(img1[y, x+(rad + 1), 2]))/4)
-        cv2.circle(img1, (x, y), rad, (int(blue), int(green), int(red)), -1)
+        blue = int((int(img1[y+rad, x, 0]) + int(img1[y-rad, x, 0]) + int(img1[y, x-rad, 0]) + int(img1[y, x+rad, 0]) + int(img1[y+cossin, x+cossin, 0]) + int(img1[y+cossin, x-cossin, 0]) + int(img1[y-cossin, x+cossin, 0]) + int(img1[y-cossin, x-cossin, 0]))/8)
+        green = int((int(img1[y+rad, x, 1]) + int(img1[y-rad, x, 1]) + int(img1[y, x-rad, 1]) + int(img1[y, x+rad, 1]) + int(img1[y+cossin, x+cossin, 1]) + int(img1[y+cossin, x-cossin, 1]) + int(img1[y-cossin, x+cossin, 1]) + int(img1[y-cossin, x-cossin, 1]))/8)
+        red = int((int(img1[y+rad, x, 2]) + int(img1[y-rad, x, 2]) + int(img1[y, x-rad, 2]) + int(img1[y, x+rad, 2]) + int(img1[y+cossin, x+cossin, 2]) + int(img1[y+cossin, x-cossin, 2]) + int(img1[y-cossin, x+cossin, 2]) + int(img1[y-cossin, x-cossin, 2]))/8)
+        cv2.circle(img1, (x, y), rad - 1, (int(blue), int(green), int(red)), -1)
         cv2.imshow('After', img1)
 
 cv2.imshow('Before', img2)
