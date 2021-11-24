861c43db4b6453a5e9a853ee9c79f9bf6f54f793
chulnwza <64070019@it.kmitl.ac.th>
2021-11-24 16:57:24 +0700

    Sharpness function

--- photoeditor.py
+++ photoeditor.py
@@ -1,12 +1,26 @@
-from PIL import Image
+"""edit photo"""
+
+from PIL import Image, ImageEnhance
 import os
 
-def formatindustry(img1, lastnamefile):
+def formatindustry(path, lastnamefile):
     """factory"""
-    for item in os.listdir('edit/input'):
+    for item in os.listdir('%s' %path):
         if item.endswith('.jpg'):
             img1 = Image.open('edit/input/%s' %item)
-            # filename, extension = os.path.splitext(item)
             img1.save('%s.%s' %(item, lastnamefile))
 
-formatindustry('edit/input/')
+# formatindustry('edit/input/')
+
+def sharpness(imagepath, sharpn):
+    """sharpness function"""
+    img1 = Image.open(imagepath)
+    enhancer = ImageEnhance.Sharpness(img1)
+    enhancer.enhance(sharpn).save('edit/output/2.jpg')
+
+# sharpness('edit/input/acne1.jpg', 3)
+    # Sharpn
+    # 0 : blurry
+    # 1 : original image
+    # 2 : increased sharpness
+    # 3 : increased more sharpness
