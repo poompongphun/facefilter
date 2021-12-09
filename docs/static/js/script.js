30efcfe3ff5b235f3c8bdfaf5bd3a9b4f45c196c
YolradeeIT <64070089@kmitl.ac.th>
2021-12-09 20:34:46 +0700

    .

--- docs/static/js/script.js
+++ docs/static/js/script.js
@@ -1,5 +1,6 @@
 const navja = document.getElementById('navbarnaja')
 
+// function if u scroll mouse down on website navbar will have shadow
 function shadow(){
     navja.style.boxShadow = `0 10px 10px -2px rgba(163, 163, 163, ${window.scrollY > 0 ? 0.2 : 0})`;
 }
