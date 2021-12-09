c53d4ab639aa3c9c4accd90c06b4c3c5dc2eedac
Poom <poompongphun@gmail.com>
2021-12-09 21:41:02 +0700

    add comments

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 origins = ["*"]
 
-app.mount("/static", StaticFiles(directory="docs/static"), name="static")
-templates = Jinja2Templates(directory="docs")
+app.mount("/static", StaticFiles(directory="docs/static"), name="static") # set path for static file
+templates = Jinja2Templates(directory="docs") # choose directory to view webpage
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,29 +21,30 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) # allow all request
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse) # make route / to show webpage
 async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request}) # response index.html to client
 
-@app.post("/acne")
+@app.post("/acne") # this route is for clear acne
 async def read_root(img: UploadFile = File(...), debug: Optional[bool] = False, conf: Optional[float] = 1, width: Optional[int] = 30):
-    contents = await img.read()
-    if contents:
-        nparr = np.fromstring(contents, np.uint8)
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        img_byte = bytes(cv2.imencode('.png', clearAcne(img, debug, conf, width))[1])
-        return Response(content=img_byte, media_type="image/webp")
+    # this route receive key: img, debug, conf, width
+    contents = await img.read() # read file from req
+    if contents: # check is contents ready to process???
+        nparr = np.fromstring(contents, np.uint8) # convert image to uint8
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # decode img uint8 format to numpy array
+        img_byte = bytes(cv2.imencode('.png', clearAcne(img, debug, conf, width))[1]) # return clean face image then convert to bytes format
+        return Response(content=img_byte, media_type="image/webp") # response image type webp to client
     else:
-        raise HTTPException(status_code=400, detail="Please select images")
+        raise HTTPException(status_code=400, detail="Please select images") # if no file in 'img' key will response error
 
 def clearAcne(img, debug=False, conf=1, width=30):
     """checkposition"""
-    acne_cascade = cv2.CascadeClassifier('cascade/5000cascade.xml')
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    mask = np.zeros_like(img)
-    faceDetected, rejectLevels, levelWeights = acne_cascade.detectMultiScale3(gray_img, 4, 3, outputRejectLevels = 1)
+    acne_cascade = cv2.CascadeClassifier('cascade/5000cascade.xml') # get cascade file
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert img to gray color
+    mask = np.zeros_like(img) # convert img to black color for mask!!!
+    faceDetected, rejectLevels, levelWeights = acne_cascade.detectMultiScale3(gray_img, 4, 3, outputRejectLevels = 1) # call cascade to detect acne
     margin = 5 # margin between detect and remove
     for i in range(len(faceDetected)):
         (x, y, w, h) = faceDetected[i] # face position
@@ -55,12 +56,12 @@ def clearAcne(img, debug=False, conf=1, width=30):
     removed_acne = cv2.inpaint(img, cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY), 3, cv2.INPAINT_NS)
     # if you want to debug -> Ctrl + /
     if debug:
-        for i in range(len(faceDetected)):
+        for i in range(len(faceDetected)): # loop all detected position
             (x, y, w, h) = faceDetected[i]
-            color = (255, 0, 0) if levelWeights[i] >= conf and w <= width else (0, 0, 255)
-            cv2.rectangle(removed_acne, (x, y), (x + w, y + h), color, 2)
-            cv2.putText(removed_acne, "Cfd: %.5f" % levelWeights[i], (x, y-5), cv2.FONT_ITALIC, 0.3, color, 1)
-            cv2.putText(removed_acne, "Wdt: %d" % w, (x, y + h + 10), cv2.FONT_ITALIC, 0.3, color, 1)
+            color = (255, 0, 0) if levelWeights[i] >= conf and w <= width else (0, 0, 255) # if low confidense color will change to red
+            cv2.rectangle(removed_acne, (x, y), (x + w, y + h), color, 2) # create reatangle around detected area
+            cv2.putText(removed_acne, "Cfd: %.5f" % levelWeights[i], (x, y-5), cv2.FONT_ITALIC, 0.3, color, 1) # show confidense text
+            cv2.putText(removed_acne, "Wdt: %d" % w, (x, y + h + 10), cv2.FONT_ITALIC, 0.3, color, 1) # show size text
     return removed_acne
 
 def getAvgColor(img, position, rlong, num, channel):
